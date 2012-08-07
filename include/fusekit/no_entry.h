abad3385341e59ee1da9085bdcb2bea37009bc06
gelipp <gelipp@20e8d64a-733d-33ea-a72f-2ae03f9e5fb2>
2012-08-07 07:17:34 +0000

    License changed to MIT

--- include/fusekit/no_entry.h
+++ include/fusekit/no_entry.h
@@ -1,21 +1,3 @@
-/*
- * This file is part of fusekit, a c++ convenience wrapper for FUSE.
- *
- * Written by Gerhard Lipp <gerhard_lipp@gmx.de>
- *
- * this library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public 
- * License along with fusekit.  If not, see <http://www.gnu.org/licenses/>.
- */
 
 #ifndef __FUSEKIT__NO_ENTRY_H
 #define __FUSEKIT__NO_ENTRY_H
